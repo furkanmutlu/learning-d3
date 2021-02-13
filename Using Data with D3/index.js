b1dd77ea3c06556eab0d495b250e80b80255bed3
Furkan Mutlu <furkanmutluu@gmail.com>
2021-02-13 15:13:08 +0100

    Using Data with D3

---
+++ Using Data with D3/index.js
@@ -0,0 +1,17 @@
+const svg = d3.select('svg');
+
+d3
+    .json('planets.json')
+    .then(data => {
+        const circs = svg
+            .selectAll('circle')
+            .data(data);
+
+        circs
+            .enter()
+            .append('circle') 
+            .attr('cy', 200)
+            .attr('cx', d => d.distance)
+            .attr('r', d => d.radius)
+            .attr('fill', d => d.fill);   
+    });
